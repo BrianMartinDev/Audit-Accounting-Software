e4b5488828746d98b0040e4a4198ed8dab43c034
Brian Martin <46796447+BrianMartinDev@users.noreply.github.com>
2020-02-27 23:51:15 -0500

    initial commit
    initial commit

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\BMART\PycharmProjects\AuditAccountingSoftware\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
